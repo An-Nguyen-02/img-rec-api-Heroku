3cb423e8b59f57892b9b3161e01dab72c28cd09f
An-Nguyen-02 <nhatan020902@gmail.com>
2021-12-30 20:01:40 +0200

    fixing cors

--- sever.js
+++ sever.js
@@ -16,7 +16,7 @@ const db = knex({
 const app = express();
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:3000/", credentials: true }))
+app.use(cors({ origin: "https://mysterious-hollows-03588.herokuapp.com/", credentials: true }))
 const bcrypt = require('bcryptjs');
 
 app.get('/',(req, res)=>{
