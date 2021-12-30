86267122d496221a44af32f2d5cf3a1c8a4749c8
An-Nguyen-02 <nhatan020902@gmail.com>
2021-12-30 20:17:07 +0200

    fixing cors

--- sever.js
+++ sever.js
@@ -14,19 +14,8 @@ const db = knex({
 });
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-const whitelist = ["http://localhost:3000"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions))
 //app.use(cors({ origin: "http://localhost:3000", credentials: true }))
 const bcrypt = require('bcryptjs');
 
